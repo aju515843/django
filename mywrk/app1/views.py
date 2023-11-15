88a4f3b474f1a62ad28e68292b4f51a187ea8378
Ajith AR <83577676+aju515843@users.noreply.github.com>
2023-11-15 08:54:14 +0530

    work

--- mywrk/app1/views.py
+++ mywrk/app1/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from app1.models import *
 
 def home(request):
-    return render(request,'home.html')
+    # d={'name':'Alex','age':20}
+    d=student.objects.all()
+    return render(request,'home.html',{'d':d})
 
 def index(request):
     return HttpResponse("hello welcome to this page")
