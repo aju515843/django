773b251fc1849666b1aad4730c8d070d182a3d5d
Ajith AR <83577676+aju515843@users.noreply.github.com>
2023-11-29 08:48:23 +0530

    work

---
+++ work5/app1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from app1.models import *
+from app1.form import *
+# Create your views here.
+
+def base(request):
+    return render(request,'base.html')
+
+def upload(request):
+    form=bookform()
+    if(request.method=="POST"):
+        form=bookform(request.POST,request.FILES)
+        if(form.is_valid()):
+            form.save()
+            return base(request)
+    return render(request,'add_book.html',{'form':form})
+
+def booklist(request):
+    b=Book.objects.all()
+    return render(request,'list.html',{'b':b})
+
